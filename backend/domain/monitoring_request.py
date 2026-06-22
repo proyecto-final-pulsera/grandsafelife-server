222fca11576e0e70f3860c89e7fff3eeed5bff69
nalmaraz <nalmaraz@frba.utn.edu.ar>
2026-06-21 21:30:24 -0300

    Fix inconsistencia domain

--- backend/domain/monitoring_request.py
+++ backend/domain/monitoring_request.py
@@ -1,52 +1,70 @@
 class MonitoringRequest:
     """
-    Solicitud pendiente entre dos usuarios.
+    Solicitud pendiente entre usuarios.
 
     requester_user_id:
         Usuario que genera la solicitud.
 
-    target_user_id:
-        Usuario que debe aceptarla o rechazarla.
+    elderly_user_id:
+        Usuario anciano involucrado.
 
-    requested_role:
-        Rol que tendrá el solicitante si la solicitud es aceptada.
+    requested_user_id:
+        Usuario al que se le solicita participar
+        como admin o monitor.
+
+    requested_role: 
+        Persona que monitorea
+        admin | monitor
+
+    elderly_status:
+        Estado de confirmacion del anciano
+        pending | accepted | rejected
+
+    requested_user_status:
+        Estado de confirmacion de la persona que monitorea
+        pending | accepted | rejected
 
     status:
-        - pending
-        - accepted
-        - rejected
-
-    elderly_read:
-        Confirmacion de lectura notificacion
+        Estado general del request
+        pending | accepted | rejected
     
+    elderly_read:
+        Confrmación de lectura de app anciano 
+        True | False
+        
     requested_user_read:
-        Confirmacion de lectura notificacion
-    """    
+        Confrmación de lectura de app monitor
+        True | False
+    """
+
     def __init__(
         self,
         request_id=None,
-        
-        #Usuarios involucrados
+
         requester_user_id=None,
-        target_user_id=None,
+        elderly_user_id=None,
+        requested_user_id=None,
+
         requested_role="",
 
-        #Estado confirmacion
         elderly_status="",
-        monitor_status="",
+        requested_user_status="",
         status="",
 
-        #Confirmacion lectura
-        elderly_read = False,
-        requested_user_read = False,
-
+        elderly_read=False,
+        requested_user_read=False
     ):
         self.request_id = request_id
+
         self.requester_user_id = requester_user_id
-        self.target_user_id = target_user_id
+        self.elderly_user_id = elderly_user_id
+        self.requested_user_id = requested_user_id
+
         self.requested_role = requested_role
+
         self.elderly_status = elderly_status
-        self.monitor_status = monitor_status
+        self.requested_user_status = requested_user_status
         self.status = status
-        self.elderly_read = elderly_read 
+
+        self.elderly_read = elderly_read
         self.requested_user_read = requested_user_read
