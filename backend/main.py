58bb027dae808954d370a8d1bb2ab31e21a474f6
nalmaraz <nalmaraz@frba.utn.edu.ar>
2026-06-21 21:22:47 -0300

    Corrijo import de db desde main

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from app_http.app_http import create_http_app
 from system.system import System
-from postgres.database import DataBase
+from backend.database.usersDB.database import DataBase
 
 db = DataBase()
 system = System(db)
